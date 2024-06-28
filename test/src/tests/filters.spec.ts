0e641a9209ad72aa35bd8bb8003b7d778890e471
Deepsn <75620590+Deepsn@users.noreply.github.com>
2024-06-28 14:20:51 -0300

    feat(test): add custom and object ignore  filter test

--- test/src/tests/filters.spec.ts
+++ test/src/tests/filters.spec.ts
@@ -3,6 +3,7 @@
 import { Tracer } from "@rbxts/tracer";
 import { END_POS, START_POS } from "../constants";
 import { createPart } from "utils/part-utils";
+import type { TraceResult } from "../types";
 
 export = () => {
   it("should filter out tagged parts", () => {
@@ -15,5 +16,22 @@ export = () => {
 
     expect(result.hit).to.equal(part);
     expect(result2.hit).to.equal(undefined);
+  });
+
+  it("should ignore part", () => {
+    const part = createPart(END_POS);
+    const result = Tracer.ray(START_POS, END_POS).ignoreObject(part).run();
+
+    expect(result.hit).to.equal(undefined);
+  });
+
+  it("should use custom filter", () => {
+    const part = createPart(END_POS);
+    const filterPart = (result: TraceResult) => {
+      return result.hit === part;
+    };
+    const result = Tracer.ray(START_POS, END_POS).addFilter(filterPart).run();
+
+    expect(result.hit).to.equal(undefined);
   });
 };
