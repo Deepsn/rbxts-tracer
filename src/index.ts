a01d5c7d95b5684070093801241a90fd0047b345
Deepsn <75620590+Deepsn@users.noreply.github.com>
2024-06-28 11:01:17 -0300

    refactor: simplify direction calculation

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,20 @@
 import { Ray } from "./ray";
 
+function calculateDirection(startPos: Vector3, ...args: [Vector3, number?]) {
+  const [vectorPos, length] = args;
+  return length !== undefined ? vectorPos.mul(length) : vectorPos.sub(startPos);
+}
+
 export namespace Tracer {
   export function ray(startPos: Vector3, endPos: Vector3): Ray;
   export function ray(startPos: Vector3, direction: Vector3, length: number): Ray;
   export function ray(startPos: Vector3, ...args: [Vector3, number?]): Ray {
-    let calculatedDirection: Vector3 | undefined;
-    let endPos: Vector3 | undefined;
-    let direction: Vector3 | undefined;
-    let length: number | undefined;
+    const direction = calculateDirection(startPos, ...args);
 
-    if (args.size() === 1) {
-      endPos = args[0];
-    } else {
-      direction = args[0];
-      length = args[1];
-    }
-
-    if (endPos) {
-      calculatedDirection = endPos.sub(startPos);
-    } else if (direction && length) {
-      calculatedDirection = direction.mul(length);
-    }
-
-    if (!calculatedDirection) {
+    if (!direction) {
       error("Invalid arguments");
     }
 
-    return new Ray(startPos, calculatedDirection);
+    return new Ray(startPos, direction);
   }
 }
